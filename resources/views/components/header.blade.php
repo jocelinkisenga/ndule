15616adb27068b0678aa9d0802acf3c81cf1747b
kisenga shuka jocelin <72705409+jocelinkisenga@users.noreply.github.com>
2026-05-27 15:06:15 +0200

    Refactor header to fetch latest song and clean markup

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,36 +1,68 @@
 @php
     use App\Models\Song;
-    $last = Song::latest()->limit(1)->get();
+
+    // Récupère le dernier son
+    $last = Song::latest()->first();
 @endphp
+
 <div class="ms_header">
     <div class="ms_top_left">
+
         <div class="ms_top_search">
             <input type="text" class="form-control" placeholder="Search Music Here..">
+
             <span class="search_icon">
-                <img src="{{asset('clients/images/svg/search.svg')}}" alt="">
+                <img src="{{ asset('clients/images/svg/search.svg') }}" alt="">
             </span>
         </div>
+
         <div class="ms_top_trend">
-            <span><a href="#"  class="ms_color">A la une :</a></span> <span class="top_marquee"><a href="#">{{$last[0]->title}}</a></span>
+            <span>
+                <a href="#" class="ms_color">A la une :</a>
+            </span>
+
+            <span class="top_marquee">
+                <a href="#">
+                    {{ $last->title ?? 'Aucun titre disponible' }}
+                </a>
+            </span>
         </div>
+
     </div>
+
     <div class="ms_top_right">
 
-        {{-- <div class="ms_top_btn">
+        {{-- 
+        <div class="ms_top_btn">
+
             @guest
-            <a href="{{route('register')}}" class="ms_btn reg_btn" data-toggle="modal" data-target="#myModal"><span>inscription</span></a>
-            <a href="{{route('login')}}" class="ms_btn login_btn" data-toggle="modal" data-target="#myModal1"><span>connexion</span></a>
+                <a href="{{ route('register') }}" class="ms_btn reg_btn">
+                    <span>inscription</span>
+                </a>
+
+                <a href="{{ route('login') }}" class="ms_btn login_btn">
+                    <span>connexion</span>
+                </a>
             @endguest
+
             @auth
-            <div class="ms_top_lang">
-                <form action="{{route('logout')}}" method="post">
-                @csrf
-                <button type="submit"  class="ms_btn login_btn">Deconnexion</button>
-                </form>
-                {{-- <span data-toggle="modal" data-target="#lang_modal">languages <img src="images/svg/lang.svg" alt=""></span> 
-            </div>
-            <a href="{{route('login')}}" class="ms_btn login_btn" data-toggle="modal" data-target="#myModal1"><span>profile</span></a>
+                <div class="ms_top_lang">
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+
+                        <button type="submit" class="ms_btn login_btn">
+                            Déconnexion
+                        </button>
+                    </form>
+                </div>
+
+                <a href="{{ route('login') }}" class="ms_btn login_btn">
+                    <span>profile</span>
+                </a>
             @endauth
-        </div> --}}
+
+        </div>
+        --}}
+
     </div>
 </div>
